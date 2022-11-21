33afe695167a0f2e6c90d3ba530d27ffa7e5fcf1
Athulraj M P <athulrajmoosraippurakkal@gmail.com>
2022-11-21 19:31:06 +0530

    All js code set for finding polynomial

--- script.js
+++ script.js
@@ -1,58 +1,37 @@
-var exp = '2(x*x)+5(x)-1375'
-
-var helperBtns = document.querySelectorAll('.hepler-btns');
+// doms into js
 var input = document.querySelector('#equation');
 
+//button onclick functions
+const clicked = text => { input.value += text }
 
-function clicked(text) {
-  input.value+=text
-}
+const bkspc = () => { input.value = input.value.slice(0, -1) }
 
-function bkspc() {
-  input.value=input.value.slice(0,-1)
-}
+const clr = () => { input.value = "" }
 
-  
-function clr(){
-  input.value = "" 
-}
-
-function calc() {
-  results = solve(findTerms(input.value))
+const calc = () => {
+  results = solve(findTerms(termConfig(input.value)));
   document.querySelector("#result").value = `x = { ${results[0]} , ${results[1]} }`;
-  
 }
 
-
-
-
-function findTerms(exp) {
-  terms = [];
-  initSplit = exp.split("+");
-
-  if (initSplit.length < 3) {
-    initSplit.forEach((term) => {
-      if (term.includes("-")) {
-        term = term.split("-");
-        terms.push(term[0]);
-        terms.push("-"+term[1]);
-      } else {
-        terms.push(term)
-      }
-    });
-  } else {
-    terms = initSplit
-  }
-  return terms
+//finding solutions
+const termConfig = exp => {
+  newExp = ''
+  for (i in exp) {exp[i] == "-" && exp[i - 1] == "x" ? (newExp += "+-") : (newExp += exp[i]);}
+  return newExp
 }
 
-function solve(terms) {
+const findTerms = exp => { return exp.split("+") }
+
+const solve = terms => {
+  //spliting numbers from all terms
   a = parseInt(terms[0].split('(')[0])
   b = parseInt(terms[1].split('(')[0])
   c = parseInt(terms[2])
-
+  
+  //finding two solutions
   x1 = (-b + Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
   x2 = (-b - Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
 
+  // returning solutions as a list
   return [x1,x2]
 }
