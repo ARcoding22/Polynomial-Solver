7cb6ea69d2de0ccd09e5bc05287b24de5284e157
athulraj-coding <athulrajmoosarippurakkal@gmail.com>
2022-11-23 23:01:34 +0530

    Steps feature added

--- script.js
+++ script.js
@@ -24,9 +24,9 @@ const findTerms = exp => { return exp.split("+") }
 
 const solve = terms => {
   //spliting numbers from all terms
-  a = parseInt(terms[0].split('(')[0])
-  b = parseInt(terms[1].split('(')[0])
-  c = parseInt(terms[2])
+  a = parseInt(terms[0]);
+  b = parseInt(terms[1]);
+  c = parseInt(terms[2]);
   
   //finding two solutions
   x1 = (-b + Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
@@ -34,4 +34,44 @@ const solve = terms => {
 
   // returning solutions as a list
   return [x1,x2]
+}
+
+const showSteps = () => {
+  if (input.value == '') return
+  terms = findTerms(termConfig(input.value));
+
+  if (terms.length != 3) return
+  
+  a = parseInt(terms[0]);
+  b = parseInt(terms[1]);
+  c = parseInt(terms[2]);
+
+  discriminent = Math.sqrt(b**2-(4*a*c))
+
+  txt = `
+  a = ${a}
+  b = ${b}
+  c = ${c}</br>
+  &radic;(b<sup>2</sup>-4ac) = &radic;${b}<sup>2</sup>-4&times;${a}&times;${c}
+  &radic;(b<sup>2</sup>-4ac) = &radic;${b ** 2}-${4 * a * c}
+  &radic;(b<sup>2</sup>-4ac) = &radic;${b ** 2 - 4 * a * c}
+  &radic;(b<sup>2</sup>-4ac) = ${discriminent}
+  discriminent = ${discriminent}</br>
+  <span class="sol">&#119909 = <span class="rhs"><i style="border-bottom: 2px solid currentColor;">-b &plusmn; &radic;b<sup>2</sup>-4ac</i><i>2a</i></span></span>
+  <span class="sol">&#119909 = <span class="rhs"><i style="border-bottom: 2px solid currentColor;">-${b} &plusmn; &radic;${b}<sup>2</sup>-4&times;${a}&times;${c}</i><i>2&times;${a}</i></span></span>
+  <span class="sol">&#119909 = <span class="rhs"><i style="border-bottom: 2px solid currentColor;">-${b} &plusmn; &radic;${b ** 2}-${4 * a * c}</i><i>${2*a}</i></span></span>
+  <span class="sol">&#119909 = <span class="rhs"><i style="border-bottom: 2px solid currentColor;">-${b} &plusmn; &radic;${b ** 2-(4 * a * c)}</i><i>${2*a}</i></span></span>
+  <span class="sol">&#119909 = <span class="rhs"><i style="border-bottom: 2px solid currentColor;">-${b} &plusmn; ${Math.sqrt(b ** 2-(4 * a * c))}</i><i>${2*a}</i></span></span>
+  <span class="sol">&#119909 = {<span class="rhs"><i style="border-bottom: 2px solid currentColor;">-${b} + ${Math.sqrt(b ** 2-(4 * a * c))}</i><i>${2*a}</i></span> , <span class="rhs"><i style="border-bottom: 2px solid currentColor;">-${b} - ${Math.sqrt(b ** 2-(4 * a * c))}</i><i>${2*a}</i></span>}</span>
+  <span class="sol">&#119909 = {<span class="rhs"><i style="border-bottom: 2px solid currentColor;">${-b+Math.sqrt(b ** 2-(4 * a * c))}</i><i>${2*a}</i></span> , <span class="rhs"><i style="border-bottom: 2px solid currentColor;">${-b-Math.sqrt(b ** 2-(4 * a * c))}</i><i>${2*a}</i></span>}</span>
+  <span class="sol">&#119909 = {<span class="rhs"><i>${solve(terms)[0]}</i></span> , <span class="rhs"><i>${solve(terms)[1]}</i></span>}</span>`;
+
+  newTxt = ''
+
+  for (ltr in txt) { (txt[ltr] == txt[0]) ? newTxt+='</br>' : newTxt+=txt[ltr] }
+  document.querySelector(".steps").innerHTML = newTxt;
+  document.querySelector('.steps').style.width = input.clientWidth+'px';
+  
+  document.querySelector('.steps').classList.toggle('hide');
+  
 }
